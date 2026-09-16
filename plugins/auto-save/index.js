24fceb3d917c708da90fad55686ccf312bf23115
zgy-beep <419172721@qq.com>
2026-09-16 17:14:14 +0800

    fix: v1.8.1 强化文字三级层级（重点全色加粗/解释缩小灰化，分区标题升级）

--- plugins/auto-save/index.js
+++ plugins/auto-save/index.js
@@ -1,5 +1,5 @@
 /**
- * SillyTavern 聊天小说连载阅读服务端插件 (v1.8.0)
+ * SillyTavern 聊天小说连载阅读服务端插件 (v1.8.1)
  * 
  * 文件路径：plugins/auto-save/index.js
  * 
@@ -243,7 +243,7 @@ async function init(router) {
         res.json({
             ready: true,
             plugin: pluginName,
-            version: '1.8.0',
+            version: '1.8.1',
             logsDir: LOGS_DIR
         });
     });
